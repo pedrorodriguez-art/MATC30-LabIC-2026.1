452c4db1c0e391720338b663790e496d819f8210
marquesleticia94-jpg <marquesleticia94@gmail.com>
2026-06-26 20:20:52 -0300

    Add files via upload

---
+++ Leticia Marques/Atividade-01/atividade_01.py
@@ -0,0 +1,7 @@
+def soma(a,b):
+    return a+b
+
+if __name__=="__main__":
+    a=int(input("Digite o primeiro número: "))
+    b=int(input("Digite o segundo número: "))
+    print("Resultado:", soma(a,b))

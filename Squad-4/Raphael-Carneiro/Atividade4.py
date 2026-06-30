94cc09a8f73fdf33f3ccb2f4c9c91ffe48b50d77
RaphaelCarneiro <raphaelcarneiroufba@gmail.com>
2026-06-30 19:45:51 -0300

    Update Atividade4.py

--- Squad-4/Raphael-Carneiro/Atividade4.py
+++ Squad-4/Raphael-Carneiro/Atividade4.py
@@ -1,20 +1,37 @@
 def organizingContainers(container):
     n = len(container)
-    container_capacities = [sum(row) for row in container]
-    ball_type_totals = [sum(container[i][j] for i in range(n)) for j in range(n)]
-    container_capacities.sort()
-    ball_type_totals.sort()
-    if container_capacities == ball_type_totals:
-        return "Possible"
-    else:
-        return "Impossible"
+    
+    container_capacities = [0] * n
+    for i in range(n):
+        row_sum = 0
+        for j in range(n):
+            row_sum += container[i][j]
+        container_capacities[i] = row_sum
 
-if __name__ == '__main__':
-    q = int(input().strip())
-    for _ in range(q):
-        n = int(input().strip())
-        container = []
-        for _ in range(n):
-            container.append(list(map(int, input().rstrip().split())))
-        result = organizingContainers(container)
-        print(result)
+    ball_types_count = [0] * n
+    for j in range(n):
+        col_sum = 0
+        for i in range(n):
+            col_sum += container[i][j]
+        ball_types_count[j] = col_sum
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if container_capacities[j] > container_capacities[j + 1]:
+                temp = container_capacities[j]
+                container_capacities[j] = container_capacities[j + 1]
+                container_capacities[j + 1] = temp
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if ball_types_count[j] > ball_types_count[j + 1]:
+                # Troca manual
+                temp = ball_types_count[j]
+                ball_types_count[j] = ball_types_count[j + 1]
+                ball_types_count[j + 1] = temp
+
+    for i in range(n):
+        if container_capacities[i] != ball_types_count[i]:
+            return "Impossible"
+            
+    return "Possible"

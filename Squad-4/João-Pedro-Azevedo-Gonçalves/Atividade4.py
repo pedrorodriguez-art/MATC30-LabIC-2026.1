84be5b144c4ef8c855f993dfe29d88484dc2535a
jjpedro29 <azevedojoaopedro256@gmail.com>
2026-07-17 15:21:11 -0300

    Create Atividade4.py

---
+++ Squad-4/João-Pedro-Azevedo-Gonçalves/Atividade4.py
@@ -0,0 +1,57 @@
+def organizingContainers(container):
+    n = len(container)
+    
+    container_capacities = [0] * n
+    for i in range(n):
+        row_sum = 0
+        for j in range(n):
+            row_sum += container[i][j]
+        container_capacities[i] = row_sum
+
+    ball_types_count = [0] * n
+    for j in range(n):
+        col_sum = 0
+        for i in range(n):
+            col_sum += container[i][j]
+        ball_types_count[j] = col_sum
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if container_capacities[j] > container_capacities[j + 1]:
+                temp = container_capacities[j]
+                container_capacities[j] = container_capacities[j + 1]
+                container_capacities[j + 1] = temp
+
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if ball_types_count[j] > ball_types_count[j + 1]:
+                temp = ball_types_count[j]
+                ball_types_count[j] = ball_types_count[j + 1]
+                ball_types_count[j + 1] = temp
+
+    for i in range(n):
+        if container_capacities[i] != ball_types_count[i]:
+            return "Impossible"
+            
+    return "Possible"
+
+
+def main():
+    caso_1 = [
+        [1, 1],
+        [1, 1]
+    ]
+    resultado_1 = organizingContainers(caso_1)
+    print(resultado_1)
+    
+    caso_2 = [
+        [0, 2],
+        [1, 1]
+    ]
+    resultado_2 = organizingContainers(caso_2)
+    print(resultado_2)
+
+
+if __name__ == "__main__":
+    main()
+
